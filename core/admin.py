48045440f5d9ad246d9ef026aa17057af6f6f896
gustavokubiack <guukubiack@gmail.com>
2022-08-29 12:22:11 -0300

    criação das models de autor e livro

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from core.models import Categoria, Editora
+from core.models import Categoria, Editora, Autor, Livro
 
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Autor)
+admin.site.register(Livro)
+
+
+
