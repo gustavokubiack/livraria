6fbcf6787df16ed41e298808c1cac1316c00c199
gustavokubiack <guukubiack@gmail.com>
2022-10-20 16:34:35 -0300

    configurações na urls

--- livraria/urls.py
+++ livraria/urls.py
@@ -27,10 +27,14 @@ router.register(r"autor", AutorViewSet)
 
 
 urlpatterns = [
+    # Admin
     path("admin/", admin.site.urls),
+    # Token
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    # Imagens
     path("api/media/", include(media_router.urls)),
+    # OPEN API 3
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/swagger/",
@@ -42,6 +46,7 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
+    # API REST
     path("api/", include(router.urls)),
 ]
 
