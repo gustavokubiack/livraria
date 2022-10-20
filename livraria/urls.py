31f4203822909c219febf980e3516d389a7e75b5
gustavokubiack <guukubiack@gmail.com>
2022-10-20 16:38:04 -0300

    teste na urls

--- livraria/urls.py
+++ livraria/urls.py
@@ -27,14 +27,10 @@ router.register(r"autor", AutorViewSet)
 
 
 urlpatterns = [
-    # Admin
     path("admin/", admin.site.urls),
-    # Token
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    # Imagens
     path("api/media/", include(media_router.urls)),
-    # OPEN API 3
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/swagger/",
@@ -46,7 +42,6 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-    # API REST
     path("api/", include(router.urls)),
 ]
 
